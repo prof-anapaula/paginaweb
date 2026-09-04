e561692c4c14d5d3241dddca8510015df7afeed8
Ana Paula <115089668+prof-anapaula@users.noreply.github.com>
2026-09-03 21:57:54 -0300

    javascript temaescuro

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function botaoclicado() {
     let texto = botao.querySelector("span");
     texto.textContent++;
 }
+
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+btnTemaEscuro.addEventListener("click", mudaTema);
+function mudaTema() {
+    const corpoPagina = document.body;
+
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
+}
